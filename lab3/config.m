952bb0904bdb94a737a9f8bbc357843c51a00422
Nikita Popov <nikita29042001@yandex.ru>
2022-09-13 18:08:40 +0300

    Update config.m

--- lab3/config.m
+++ lab3/config.m
@@ -4,23 +4,27 @@ a_0 = -10;
 a_1 = -1;
 b_0 = 3;
 
-% a_m_0 = ;
-% a_m_1 = ;
-
 A = [   0    1;
      -a_0 -a_1];
 b = [0;
      b_0];
 
-A_m = [     0      1;
-       -a_m_0 -a_m_1];
-b_m = [0;
-       a_m_0];
+% A_m = [     0      1;
+%        -a_m_0 -a_m_1];
+% b_m = [0;
+%        a_m_0];
 
 theta_1 = (a_0 - a_m_0)/b_0;
 theta_2 = (a_1 - a_m_1)/b_0;
+
 theta = [theta_1;
          theta_2];
+
+kappa = b0/a_m_0;
+
+A_m = A + b*theta';
+b_m = b/kapa;
+
 
 t_p = 0.9;
 sigma = 0.15;
