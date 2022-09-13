2f0a6bc20f1d4bfd107872618da3a7144776cd46
Nikita Popov <nikita29042001@yandex.ru>
2022-09-13 18:42:26 +0300

    Am coeffs found

--- lab3/config.m
+++ lab3/config.m
@@ -1,11 +1,14 @@
 %% Variant 12
+%Исходные данные 
+t_p = 0.9; %Переходной процесс
+sigma = 0.15; %Перерегулирование
 
 a_0 = -10;
 a_1 = -1;
 b_0 = 3;
 
-a_m_0 = -5;
-a_m_1 = -0.5;
+a_m_0 = 69;
+a_m_1 = 15;
 b_m_0 = a_m_0;
 
 
@@ -16,10 +19,12 @@ b = [0;
 
 C = [1 0];
 
-% A_m = [     0      1;
-%        -a_m_0 -a_m_1];
-% b_m = [0;
-%        a_m_0];
+A_m = [     0      1;
+       -a_m_0 -a_m_1];
+b_m = [0;
+       a_m_0];
+C_m = C;
+eig(A_m)
 
 theta_1 = (a_0 - a_m_0)/b_0;
 theta_2 = (a_1 - a_m_1)/b_0;
@@ -27,15 +32,11 @@ theta_2 = (a_1 - a_m_1)/b_0;
 theta = [theta_1;
          theta_2];
 
-kappa = b0/a_m_0;
+kappa = b_0/a_m_0;
 
-A_m = A + b*theta';
-b_m = b/kapa;
-C_m = C;
+% 
+% A_m = A + b*theta';
+% b_m = b/kapa;
+% C_m = C;
 
 
-t_p = 0.9;
-sigma = 0.15;
-
-A_m
-b_m
