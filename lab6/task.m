78af63e79f2f96c5e50e22f2731bb237065da8af
Vlad1slav <Vvlad1slavV@yandex.ru>
2022-11-01 19:23:19 +0300

    Add task Lab6

--- lab6/task.m
+++ lab6/task.m
@@ -1,8 +1,33 @@
-% 12 Вариант
-%% Исходные данные
-a0 = 3;
-a1 = 2;
-b0 = 6;
-b1 = 2;
-k0 = 0.36;
-k1 = sqrt(0.72);
+path = "out/";
+config;
+%%
+TIME = 15000;
+gamma = 1.2;
+
+sim_out = sim("model1.slx", TIME);
+
+time = sim_out.tout;
+x = squeeze(sim_out.logsout.get("x").Values.Data);
+x_hat = squeeze(sim_out.logsout.get("x_hat").Values.Data)';
+y = squeeze(sim_out.logsout.get("y").Values.Data);
+y_hat = squeeze(sim_out.logsout.get("y_hat").Values.Data);
+omega = sim_out.logsout.get("omega").Values.Data;
+eplsilon = y - y_hat;
+e = x - x_hat;
+plot(time, e)
+
+%%
+TIME = 15000;
+gamma = 0.02;
+
+sim_out = sim("model2.slx", TIME);
+
+time = sim_out.tout;
+x = squeeze(sim_out.logsout.get("x").Values.Data);
+x_hat = squeeze(sim_out.logsout.get("x_hat").Values.Data)';
+y = squeeze(sim_out.logsout.get("y").Values.Data);
+y_hat = squeeze(sim_out.logsout.get("y_hat").Values.Data);
+omega = sim_out.logsout.get("omega").Values.Data;
+eplsilon = y - y_hat;
+e = x - x_hat;
+plot(time, e)
