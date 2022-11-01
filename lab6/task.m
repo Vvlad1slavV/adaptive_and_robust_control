e6f97dd7d61b11ac6e60d8da2fd7c746c64852a2
motorrr4ik <nikita29042001@yandex.ru>
2022-11-01 20:38:45 +0300

    Update

--- lab6/task.m
+++ lab6/task.m
@@ -14,8 +14,18 @@ y_hat = squeeze(sim_out.logsout.get("y_hat").Values.Data);
 omega = sim_out.logsout.get("omega").Values.Data;
 eplsilon = y - y_hat;
 e = x - x_hat;
-plot(time, e)
+% plot(time, e)
 
+x_norm = sqrt((x-x_hat)'*(x-x_hat));
+
+task_number = 1;
+set_number = 1;
+plot(time, x_norm, "$Time, s$", "$x(t)$")
+grid on
+saveas(gcf, path+'task'+string(task_number)+'_'+string(set_number)+'.png')
+% plot_xy_continuous_lable(time, e, "$Time, s$", "$e(t)$", true)
+% saveas(gcf, path+'task'+string(task_number)+'_'+string(set_number)+'e'+'.png')
+close all;
 %%
 TIME = 15000;
 gamma = 0.02;
